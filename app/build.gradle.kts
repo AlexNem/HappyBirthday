69aac3fc1451c14a3b54d95e9c7628e252dc0837
alexnemyr <onemyrovskyi@itsmartflex.com>
2024-04-01 18:47:35 +0200

    upgrade version to 0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.alexnemyr.happybirthday"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.4"
+        versionCode = 6
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
